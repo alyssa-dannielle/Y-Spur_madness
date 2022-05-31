7778ce09ee8d467db8f8982decba9015f142c46b
alyssa-dannielle <103299483+alyssa-dannielle@users.noreply.github.com>
2022-05-31 14:00:36 -0400

    Update scrap.py
    finished adding txt files and sorted them into numerical order
    still updating via copy/paste

--- Y-Spur_Madness/scrap.py
+++ Y-Spur_Madness/scrap.py
@@ -7,21 +7,37 @@
 # SALS = 21810
 # NEWC = 19701
 # SDVR = 19901
-zip = input("Enter zip: ")
+#zip = input("Enter zip: ")
 
-if len(zip) > 5:
-    print("Enter 5 digit zip: ")
+#if len(zip) > 5:
+ #   print("Enter 5 digit zip: ")
 
-if zip == "19701":
-    print("NEWC")
-elif zip == "19901":
-    print("SDVR")
-elif zip == "21810":
-    print("SALS")
-else:
-    print("OUTBOUND")
+#if zip == "19701":
+ #   print("NEWC")
+#elif zip == "19901":
+ #   print("SDVR")
+#elif zip == "21810":
+ #   print("SALS")
+#else:
+ #   print("OUTBOUND")
 
+#  im going to try sorting the contents of the files in numerical order
+#  using some code that i found on a google search *fingers crossed
 
+zips = list()
+
+filename = 'ebal.txt'
+with open (filename) as fin:
+    for line in fin:
+        zips.append(line.strip())
+
+zips.sort()
+#print(zips)
+
+filename = 'ebal_sorted.txt'
+with open(filename, 'w') as fout:
+    for zip in zips:
+        fout.write(zip + '\n')
 
 
 
